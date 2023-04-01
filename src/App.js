f795b56f3302e53649ad7e6e389fa1579f074f7a
Insiro <leeon@insiro.me>
2023-04-01 22:40:11 +0900

    add: Centerize Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './index.css';
-import Login from './components/login'
-import Register from './components/register'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/login';
+import Register from './components/register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CenterLayout } from './components/layout';
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path="register" element={<Register/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/auth" element={<CenterLayout />}>
+                        <Route path="/auth" element={<Login />} />
+                        <Route path="/auth/register" element={<Register />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
