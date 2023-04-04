fa305ad40b5188d03f69cdfdcf07601adddfe0e4
Insiro <leeon@insiro.me>
2023-04-04 23:50:30 +0900

    add: RootRedictor based on userStore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { Provider, useSelector } from 'react-redux';
 import './index.css';
+
+import store from './stores';
+
+import { CenterLayout, NormalLayout } from './components/layout';
 import Login from './pages/singIn';
 import Register from './pages/register';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CenterLayout, NormalLayout } from './components/layout';
 import { Drons } from './pages/drons';
 import { NewDron } from './pages/newDron';
+import { Main } from './pages/main';
+const RootRedirector = () => {
+    const user = useSelector((state) => state.user.name);
+    return <Navigate to={user ? '/drons' : '/auth'} replace />;
+};
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/auth" element={<CenterLayout />}>
-                    <Route path="/auth" element={<Login />} />
-                    <Route path="/auth/register" element={<Register />} />
-                </Route>
-                <Route path="/drons" element={<NormalLayout />}>
-                    <Route path="/drons/new" element={<NewDron />} />
-                    <Route path="/drons:id" element={<Drons />} />
-                </Route>
-            </Routes>
+            <Provider store={store}>
+                <Routes>
+                    <Route path="/" element={<RootRedirector />} />
+                    <Route path="/auth" element={<CenterLayout />}>
+                        <Route path="/auth" element={<Login />} />
+                        <Route path="/auth/register" element={<Register />} />
+                    </Route>
+                    <Route path="/drons" element={<NormalLayout />}>
+                        <Route path="/drons" element={<Main />} />
+                        <Route path="/drons/new" element={<NewDron />} />
+                        <Route path="/drons:id" element={<Drons />} />
+                    </Route>
+                </Routes>
+            </Provider>
         </BrowserRouter>
     );
 }
