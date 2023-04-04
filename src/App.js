0a6d6b82ffdb79728dbf065ca6a560ddf419f10b
Insiro <leeon@insiro.me>
2023-04-04 22:33:59 +0900

    add: Add Dron link , update routers fix: label for -> htmlfor

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Register from './pages/register';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { CenterLayout, NormalLayout } from './components/layout';
 import { Drons } from './pages/drons';
-
+import { NewDron } from './pages/newDron';
 function App() {
     return (
         <BrowserRouter>
@@ -13,8 +13,9 @@ function App() {
                     <Route path="/auth" element={<Login />} />
                     <Route path="/auth/register" element={<Register />} />
                 </Route>
-                <Route path="/" element={<NormalLayout />}>
-                    <Route path="/" element={<Drons />} />
+                <Route path="/drons" element={<NormalLayout />}>
+                    <Route path="/drons/new" element={<NewDron />} />
+                    <Route path="/drons:id" element={<Drons />} />
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/components/layout/sidebar/index.jsx
+++ src/components/layout/sidebar/index.jsx
@@ -1,44 +1,42 @@
 import { SideBarWrapper } from './wrapper';
 import SidebarItem_User from './SidebarItem';
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import '../css/sidebar.css';
-
+import { Link } from 'react-router-dom';
 
 //TODO: add Sidebar contents
 export const SideBar = ({ children }) => {
-    const [newButton, setNewButton] = useState("");
+    const [newButton, setNewButton] = useState('');
     const [buttons, setButtons] = useState([]);
-  
-    // 버튼 추가 이벤트 핸들러
-    const addButton = () => {
-        if (newButton !== "") { 
-            setButtons([...buttons, newButton]);
-            setNewButton("");
-        }
-    };
-
-
 
     return (
         <SideBarWrapper>
             <div>{children}</div>
             <div>
                 <SidebarItem_User
-                image="https://pbs.twimg.com/media/FBzir4sVIAMeXby.png"
-                text="몰?루"
+                    image="https://pbs.twimg.com/media/FBzir4sVIAMeXby.png"
+                    text="몰?루"
                 />
             </div>
-            <div class="button-wrapper">
+            <div className="button-wrapper">
                 {buttons.map((button, index) => (
                     <button key={index}>{button}</button>
                 ))}
             </div>
-            <div class="sidebar-bottom">    
-            <form>
-                <label for="input-text">Drone Input:</label>
-                <input type="text" id="input-text" name="input-text"value = {newButton} onChange={(e)=>setNewButton(e.target.value)}/>
-                <button type="button"onClick={addButton}>Add</button>
-            </form>
+            <div className="sidebar-bottom">
+                <form>
+                    <label htmlFor="input-text">Drone Input:</label>
+                    <input
+                        type="text"
+                        id="input-text"
+                        name="input-text"
+                        value={newButton}
+                        onChange={(e) => setNewButton(e.target.value)}
+                    />
+                    <Link to="/drons/new">
+                        <button type="button">Add Dron</button>
+                    </Link>
+                </form>
             </div>
         </SideBarWrapper>
     );
