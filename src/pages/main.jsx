5e80bfb1667ecc5e588837213de7ca7883ae0ff6
Insiro <leeon@insiro.me>
2023-04-04 23:49:10 +0900

    add: main page

---
+++ src/pages/main.jsx
@@ -0,0 +1,9 @@
+export const Main = () => {
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <h2>TODO: Add Main Page Contents</h2>
+            <h2>Will Pe Filled</h2>
+        </div>
+    );
+};
