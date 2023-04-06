74fb67233906ca766096f9f8ef0416046ca3afb8
Insiro <leeon@insiro.me>
2023-04-06 21:20:56 +0900

    fix: sidebar resizing

--- src/components/layout/sidebar/wrapper.jsx
+++ src/components/layout/sidebar/wrapper.jsx
@@ -5,11 +5,13 @@ export const SideBarWrapper = styled.div`
     ${styles.TRANSITION_SLOW}
     max-width: 256px;
     width: 256px;
+    min-width: 256px;
+    max-width: 256px;
     height: 100%;
     display: flex;
     flex-direction: column;
     overflow-x: hidden;
     ${styles.MEDIAQUERY_UNDER_R} {
-        width: ${(props) => (props.opened ? '0px' : '256px')};
+        display: ${(props) => (props.opened ? 'flex' : 'none')};
     }
 `;
