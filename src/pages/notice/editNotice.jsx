5ff1516082a182b8dbdaac70bd80397728dcd06b
SnowArtics <84330600+SnowArtics@users.noreply.github.com>
2023-05-24 01:19:40 +0900

    FEAT : Make Notice End Point
    Make Notice End Point and design
    Add editNotice
    Add newNotice
    Add noticeFile
    Fix NoticeCard
    Fix Notice

---
+++ src/pages/notice/editNotice.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom'
+
+const BlogPostFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  text-align: center;
+  justify-content: center;
+  width : 100%
+`;
+
+const TitleInput = styled.input`
+  width: 100%;
+  max-width: 600px;
+  height: 38px;
+  margin-bottom: 20px;
+  font-size: 27px;
+  text-transform: capitalize;
+
+  @media screen and (max-width: 300px) {
+    font-size: 20px;
+    height: 30px;
+  }
+`;
+
+const ContentTextArea = styled.textarea`
+  width: 100%;
+  max-width: 600px;
+  height: 200px;
+  margin-bottom: 20px;
+  font-size: 15px;
+
+  @media screen and (max-width: 300px) {
+    height: 150px;
+  }
+`;
+
+const SubmitButton = styled.button`
+  width: 163px;
+  height: 45px;
+  background-color: #00AAA1;
+  color: #FFFFFF;
+  font-size: 15px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const BlogPostForm = () => {
+    const baseUrl = "http://localhost:8083";
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const nevigate = useNavigate();
+    const [uid, setUID] = useState('');
+
+    useEffect(() => {
+        // 서버에서 개별 포스팅 데이터 가져오는 API 호출
+        getNotices();
+    }, []);
+
+    async function getNotices() {
+        try {
+            const path = window.location.pathname;
+            const lastPart = path.substring(path.lastIndexOf('/') + 1);
+            setUID(lastPart);
+            const response = await axios.get(baseUrl + '/api/notice/' + lastPart);
+            setTitle(response.data.title);
+            setContent(response.data.content);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        await axios
+            .post(baseUrl + '/api/notice/' + uid, {
+                title: title,
+                content: content,
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        nevigate('/notice');
+    };
+
+    return (
+        <BlogPostFormWrapper>
+            <h2>공지 글 쓰기</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div><label htmlFor="title">제목:</label></div>
+                    <TitleInput
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <div><label htmlFor="content">내용:</label></div>
+                    <ContentTextArea
+                        id="content"
+                        value={content}
+                        onChange={e => setContent(e.target.value)}
+                    />
+                </div>
+                <SubmitButton type="submit">확인</SubmitButton>
+            </form>
+        </BlogPostFormWrapper>
+    );
+};
+
+export default BlogPostForm;
--- src/pages/notice/notice.jsx
+++ src/pages/notice/notice.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import axios from "axios";
+import axios from 'axios';
 
 const BlogPostsWrapper = styled.div`
   display: flex;
@@ -9,6 +9,8 @@ const BlogPostsWrapper = styled.div`
   align-items: center;
   padding: 20px;
   text-align: center;
+  height:100%;
+  overflow-y: auto; /* 스크롤 관련 스타일을 적용할 때는 overflow-y 속성을 사용합니다. */
 `;
 
 const BlogPost = styled.div`
@@ -36,45 +38,61 @@ const BlogPostContent = styled.p`
 `;
 
 const BlogPostPage = () => {
-  const baseUrl = "http://localhost:8083";
-  const [Notices, setNotices] = useState([]);
-  const nevigate = useNavigate();
+  const baseUrl = 'http://localhost:8083';
+  const [notices, setNotices] = useState([]);
+  const navigate = useNavigate();
+  const [showNewPostButton, setShowNewPostButton] = useState(true); // 새로운 공지글 작성 버튼을 보여줄지 여부를 상태로 관리
 
   useEffect(() => {
     // 서버에서 개별 포스팅 데이터 가져오는 API 호출
     getNotices();
   }, []);
 
-  if (!Notices) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    // 화면 크기 변화 감지하여 스크롤 제어
+    window.addEventListener('resize', handleResize);
+    handleResize(); // 초기 로드 시에도 스크롤 제어를 위해 호출
 
-  async function getNotices(){
-    await axios
-      .get(baseUrl + "/api/notice")
-      .then((response)=>{
-          console.log(response.data);
-          setNotices(response.data);
-      })
-      .catch((error)=>{
-          console.log(error);
-      });  
+    return () => {
+      window.removeEventListener('resize', handleResize); // 컴포넌트 언마운트 시 리스너 제거
+    };
+  }, []);
+
+  const handleResize = () => {
+    const blogPostsWrapper = document.getElementById('blog-posts-wrapper');
+    if (blogPostsWrapper) {
+      blogPostsWrapper.style.overflowY =
+        blogPostsWrapper.scrollHeight > blogPostsWrapper.clientHeight ? 'scroll' : 'auto';
+    }
+  };
+
+  async function getNotices() {
+    try {
+      const response = await axios.get(baseUrl + '/api/notice');
+      setNotices(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const handleNewPostClick = () => {
-    nevigate('/notice/new'); // 새로운 공지글 작성 페이지로 이동
+    navigate('/notice/new'); // 새로운 공지글 작성 페이지로 이동
   };
 
   return (
-    <BlogPostsWrapper>
+    <BlogPostsWrapper id="blog-posts-wrapper">
       <h2>블로그 글 목록</h2>
-      <button onClick={handleNewPostClick}>새로운 공지글 작성</button> {/* 새로운 공지글 작성 버튼 */}
-      {Notices.map(Notice => (
-        <BlogPost key={Notice.id}>
-          <BlogPostTitle to={`/notice/${Notice.uid}`}>{Notice.title}</BlogPostTitle>
-          <BlogPostContent>{Notice.content}</BlogPostContent>
+      {showNewPostButton && ( // showNewPostButton 값이 true일 때만 버튼을 렌더링
+        <button onClick={handleNewPostClick}>새로운 공지글 작성</button>
+      )}
+      <br></br>
+      {notices.map((notice) => (
+        <BlogPost key={notice.id}>
+          <BlogPostTitle to={`/notice/${notice.uid}`}>{notice.title}</BlogPostTitle>
+          <BlogPostContent>{notice.content}</BlogPostContent>
         </BlogPost>
       ))}
+      <br></br>
     </BlogPostsWrapper>
   );
 };
